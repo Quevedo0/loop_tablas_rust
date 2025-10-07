8142707ed47752cb288b9a2527100a3f6a1ef3cf
jissalvarod <gj.alvarez24@info.uas.edu.mx>
2025-10-07 11:01:52 -0700

    tablasloop_GJAR

--- main.rs
+++ main.rs
@@ -1,32 +1,24 @@
-//KEVIN SALAS -- CIENCIAS DE DATOS -- TABLAS DE MULTIPLICAR CON LOOP
+fn main() {
 
+let mut i: i8 = 0;
+loop {
+    println!("\nTabla de {}:",i);
+let mut j: i8 = 1;    
+    //break; 
 
-fn main(){
- let mut multiplicando:i8=1;
- let mut multiplicador:i8=1;
-
-    loop{
-    println!("Tabla del numero: {}",multiplicando);
-
-            loop{
-             println!("{} x {} = {}", multiplicando ,multiplicador, multiplicando * multiplicador);
-                
-                multiplicador += 1;
-
-                if multiplicador > 10{
-                    multiplicador = 0;
-                    break;
-                }
-            }
-
-            multiplicando += 1;
-
-            println!("");
-
-         if multiplicando >10{
-            break;
-            }   
-
-    }
-
+    loop {
+        
+           println!("{} x {} = {}",i, j, i*j);
+           j += 1;
+        
+    if j >= 11{
+    break;
+    } 
+}
+    i += 1;
+        if i >= 11 {
+     break;
+    
+    } 
+}
 }
